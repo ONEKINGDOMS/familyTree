ba60c6cfe428e6d4412cc81b90decd402e83d163
Zeng <zeng1990gca@gmail.com>
2017-03-23 18:21:06 +0800

    family page

--- app/controller/familyCtrl.js
+++ app/controller/familyCtrl.js
@@ -12,29 +12,75 @@
             familyCtrl.prototype.init = function () {
                 var that = this;
                 breadcrumbs.setTitle('Family');
-                that.user = {
-                    name: 'Liu',
-                };
+                // that.user = {
+                //     name: 'Liu',
+                //     relatives:[
+                //         {
+                //             id:'1',
+                //             relative:'父亲',
+                //             firstname:'Liu',
+                //             lastname:'feng',
+                //             birthdate:'1960-10-1',
+                //             status:'live'
+                //         }
+                //     ]
+                // };
+                that.user=new user();
             };
+
+            function user() {
+                this.name='LI',
+                this.relatives=[{
+                        id:'1',
+                        relative:'父亲',
+                        firstname:'Li',
+                        lastname:'feng',
+                        birthdate:'1960-10-1',
+                        status:'live',
+                        name:'Li feng'
+                    }
+                ];
+                this.setFamily();
+            }
+
+            user.prototype.setFamily=function () {
+                this.father=_.find(this.relatives,{ relative:'父亲'});
+                this.mother=_.find(this.relatives,{ relative:'母亲'});
+                this.couple=_.find(this.relatives,{ relative:'配偶'});
+                this.childrens=_.filter(this.relatives,function (item) {
+                    return item.relative=='子女';
+                });
+            }
+
+            user.prototype.addNewMember=function (member) {
+                this.relatives.push(member);
+                this.setFamily();
+            }
+
+
+
+
 
 
             familyCtrl.prototype.addNewMember = function () {
                 var that = this;
                  $uibModal.open({
-                    animation: true,
-                    ariaLabelledBy: 'modal-title',
-                    ariaDescribedBy: 'modal-body',
-                    templateUrl: 'template/memberModal.html',
-                    controller:'modalCtrl',
+                     animation: true,
+                     ariaLabelledBy: 'modal-title',
+                     ariaDescribedBy: 'modal-body',
+                     templateUrl: 'template/memberModal.html',
+                     controller:'modalCtrl',
                      controllerAs:'modal',
-                    size: 'lg',
-                    resolve: {
+                     size: 'lg',
+                     resolve: {
                         user: function () {
                             return that.user;
                         }
                     }
                 });
             };
+
+
 
             return new familyCtrl();
         }])
--- app/controller/modalCtrl.js
+++ app/controller/modalCtrl.js
@@ -4,26 +4,62 @@ angular.module('familyTree.modal', ['ngRoute'])
     .controller('modalCtrl', ['$uibModalInstance','user',function($uibModalInstance,user) {
 
         var modalCtrl=function () {
-            this.popup={};
-            this.dateOptions = {
-                formatYear: 'yy',
-                maxDate: new Date(2020, 5, 22),
-                minDate: new Date(),
-                startingDay: 1
-            };
+            var that=this;
+            that.user=user;
+            that.popup={};
+            that.staticRelationShipList=[{
+                relativeType:'父亲'
+            },{
+                relative:'母亲'
+            },{
+                relative:'配偶'
+            },{
+                relative:'子女'
+            }];
+
+
+            that.relationShipList=_.reduce(that.staticRelationShipList,function (array,item) {
+                var find=_.find(that.user.relatives,item);
+                if(find === undefined)
+                {
+                    array.push(item);
+                }
+                return array;
+            },[])
+
         };
 
-        modalCtrl.prototype.open=function () {
-            this.popup.opened = true;
-        }
-
         modalCtrl.prototype.confirm=function () {
-            $uibModalInstance.dismiss('cancel');
+           if(this.member)
+           {
+               this.member.relative=this.member.relative.replace(/\s+/, "");
+               this.user.addNewMember(this.member);
+               $uibModalInstance.dismiss('cancel');
+           }
         }
         
         modalCtrl.prototype.cancel=function () {
             $uibModalInstance.dismiss('cancel');
         }
+
+        modalCtrl.prototype.dateOptions = {
+            dateDisabled: disabled,
+            formatYear: 'yy',
+            maxDate: new Date(),
+            minDate: new Date(1900,1,1),
+            startingDay: 1
+        };
+
+        function disabled(data) {
+            var date = data.date,
+                mode = data.mode;
+            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+        };
+
+        modalCtrl.prototype.open= function() {
+            this.popup.opened = true;
+        };
+
 
         return new modalCtrl();
     }]);
