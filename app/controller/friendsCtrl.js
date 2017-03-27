6126aaefed2a1a3ad6a591bb30174ebb2c39c399
Zeng <zeng1990gca@gmail.com>
2017-03-27 19:03:57 +0800

    Add Search

--- app/controller/friendsCtrl.js
+++ app/controller/friendsCtrl.js
@@ -2,15 +2,131 @@
 'use strict';
 
 angular.module('familyTree.friends', ['ngRoute'])
-    .controller('friendsCtrl', ['$scope','$q','breadcrumbs',function($scope,$q,breadcrumbs) {
+    .controller('friendsCtrl', ['$scope','$q','breadcrumbs','$location',function($scope,$q,breadcrumbs,$location) {
 
         var friendsCtrl=function () {
-            this.init();
+            if($location.search().friendlist)
+            {
+                this.init();
+            }else if($location.search().searchBy == 'family'){
+                this.searchByFamily();
+            }else if($location.search().searchBy == 'school'){
+                this.searchBySchool();
+            }
+
         };
 
         friendsCtrl.prototype.init=function(){
             var that=this;
             breadcrumbs.setTitle('人脉邀请');
+            that.shouldShowOutgoing=false;
+            that.shouldShowRequest=false;
+            that.friendList=[{
+                id:'1',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'2',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'3',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'4',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'5',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'6',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'7',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'8',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'9',
+                name:'Uen Yu11',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            }];
+        };
+
+        friendsCtrl.prototype.searchByFamily=function(){
+            var that=this;
+            breadcrumbs.setTitle('人脉邀请');
+            that.shouldShowOutgoing=true;
+            that.shouldShowRequest=false;
+            that.friendList=[{
+                id:'1',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'2',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'3',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'4',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'5',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'6',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'7',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'8',
+                name:'Uen Yu',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            },{
+                id:'9',
+                name:'Uen Yu11',
+                position:'Application Developer',
+                profile:'../static/profile.jpg'
+            }];
+        };
+
+        friendsCtrl.prototype.searchBySchool=function(){
+            var that=this;
+            breadcrumbs.setTitle('人脉邀请');
+            that.shouldShowOutgoing=true;
+            that.shouldShowRequest=false;
             that.friendList=[{
                 id:'1',
                 name:'Uen Yu',
@@ -60,7 +176,5 @@ angular.module('familyTree.friends', ['ngRoute'])
         };
 
         return new friendsCtrl();
-
-
     }])
 }(angular));
--- app/controller/searchCtrl.js
+++ app/controller/searchCtrl.js
@@ -6,18 +6,44 @@ angular.module('familyTree.search', ['ngRoute'])
 
         var searchCtrl=function () {
             this.init();
-        }
+        };
 
         searchCtrl.prototype.init=function () {
             var that=this;
             breadcrumbs.setTitle('寻找人脉');
-            that.searchBy=$location.search().searchBy;
-        }
+            that.shouldShowType=$location.search().shouldShowType;
+            that.terms="请输入真实信息";
+            that.searchByFamily==false;
+            that.searchBySchool=false;
+        };
 
+        //doing searching get resaon from server go
         searchCtrl.prototype.search=function () {
+            var that=this;
+            if(that.searchByFamily)
+            {
+                $location.url('friends?searchBy=family');
+            }else if(that.searchBySchool)
+            {
+                $location.url('friends?searchBy=school');
+            }
+        };
 
-        }
-
+        //show different type search
+        searchCtrl.prototype.showSearch=function (typeCode) {
+            var that=this;
+            if(typeCode == 0)
+            {
+                that.searchByFamily=true;
+                that.searchBySchool=false;
+                that.shouldShowType=0;
+            }else if(typeCode == 1)
+            {
+                that.searchByFamily=false;
+                that.searchBySchool=true;
+                that.shouldShowType=0;
+            }
+        };
 
         return new searchCtrl();
     }])
